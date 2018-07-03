3901789373137857f9d21c68e05cc4d4d59a2bdf
Landen Danyluk <landendanyluk@gmail.com>
2018-07-03 16:42:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 function theBeatlesPlay(musicians, instruments) {
   var response = [];
   for (var i = 0; i < musicians.length; i++) {
-    response.push(`${musicians[i]} plays ${instruments[i]}`)
+    response.push(`${musicians[i]} plays ${instruments[i]}`);
   }
+  return response;
 }
