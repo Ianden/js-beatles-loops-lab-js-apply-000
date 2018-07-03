b3f76c761f15a4202b10cb62c8767c2764b8f303
Landen Danyluk <landendanyluk@gmail.com>
2018-07-03 16:42:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var response = [];
+  for (var i = 0; i < musicians.length; i++) {
+    response.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+}
